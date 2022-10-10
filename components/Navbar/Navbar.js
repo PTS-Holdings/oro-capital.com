4e5d15cb83c88015ef61c89bb796c9d291609faf
Omar Sakr <o.sakr@ptscorp-eg.com>
2022-10-10 13:39:49 +0200

    feat: add responsiveness to navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import oroCapitalBlackLogo from "public/assets/capitalLogo-black.svg";
 import { NavLinks } from "./navLinks";
 
+import { useState } from "react";
 import { BsFillMoonFill } from "react-icons/bs";
 
 export const Navbar = () => {
+	const [open, setOpen] = useState(false);
 	return (
 		<nav className=" py-5 ">
-			<div className="container mx-auto flex content-center justify-between">
+			<div className="container flex content-center justify-between ">
 				<div className="my-auto">
 					<Image
 						alt="Oro Capital Logo"
@@ -16,17 +18,47 @@ export const Navbar = () => {
 						width={90}
 					/>
 				</div>
-
-				<ul className="flex content-center items-center gap-5">
+				{/* Navigation Links */}
+				<ul
+					className={`absolute z-10  flex w-screen flex-col gap-6
+					 overflow-hidden bg-white transition-all duration-300 ease-in-out
+					 md:static md:w-auto md:flex-row md:content-center md:items-center ${
+							open ? "h-[90vh]} top-24" : "-top-96"
+						}`}
+				>
 					{NavLinks.map((navLink) => (
-						<li key={navLink.link}>
-							<a href={navLink.link}>
-								{navLink.displayName}
-							</a>
+						<li key={navLink.link} className="text-center">
+							<a href={navLink.link}>{navLink.displayName}</a>
 						</li>
 					))}
-					<BsFillMoonFill />
+					<li>
+						<BsFillMoonFill />
+					</li>
 				</ul>
+
+				{/* hamburger button */}
+				<div
+					className="relative z-50 my-auto flex h-7 w-7 flex-col items-center justify-between md:hidden"
+					onClick={() => {
+						setOpen(!open);
+					}}
+				>
+					<span
+						className={`h-1 w-full transform rounded-full  bg-gray-700 transition duration-300 ease-in-out ${
+							open ? "translate-y-3 rotate-45" : ""
+						}`}
+					/>
+					<span
+						className={`h-1 w-full rounded-full  bg-gray-700 transition-all duration-300 ease-in-out ${
+							open ? "w-0" : "w-full"
+						}`}
+					/>
+					<span
+						className={`h-1 w-full transform rounded-full  bg-gray-700 transition duration-300 ease-in-out ${
+							open ? "-translate-y-3 -rotate-45" : ""
+						}`}
+					/>
+				</div>
 			</div>
 		</nav>
 	);
