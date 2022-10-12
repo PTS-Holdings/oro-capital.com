d39715f0a117b160a4d214c3eb324df25fc1a38e
ALBARAA REDA <albaraareda18@gmail.com>
2022-10-12 21:13:01 +0200

    BG switch in dark mode

--- components/homePage/HeroSection.js
+++ components/homePage/HeroSection.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 import Link from "next/link";
 import oroArtWhite from "public/assets/oroArt-white.svg";
 import { BsChevronDoubleDown, BsFillMoonFill } from "react-icons/bs";
-
+import { switchBg } from "./switchBg";
 const HeroSection = () => {
 	return (
 		<>
-			<section className=" container relative flex h-[85vh] flex-col content-center items-center justify-center text-center">
+			<section className=" container relative z-30 flex h-[85vh] flex-col content-center items-center justify-center text-center">
 				<h1 className="  text-4xl font-bold md:text-6xl lg:text-7xl">
 					Invest Safe <br /> &nbsp; &nbsp; &nbsp; &nbsp; Invest
 					<span span className="text-primaryOrange">
@@ -33,8 +33,9 @@ const HeroSection = () => {
 				<BsChevronDoubleDown className="absolute bottom-[7%] animate-bounce text-6xl text-primaryOrange" />
 			</section>
 
-			<div className=" absolute top-[20%] right-[0px] -z-10 bg-fixed pl-10 opacity-70">
-				<Image src={oroArtWhite} alt="ORO ART" objectFit="cover" />
+			<div className=" absolute top-[20%] right-[0px] -z-10 bg-fixed	 pl-10 opacity-70 dark:z-10">
+				{/* <Image src={oroArtWhite} alt="ORO ART" objectFit="cover" /> */}
+				{switchBg()}
 			</div>
 		</>
 	);
