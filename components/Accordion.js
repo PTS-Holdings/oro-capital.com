d6305e437f8869d8614b36e9c19ea6b097355ea0
Omar Sakr <o.sakr@ptscorp-eg.com>
2022-10-13 23:41:05 +0200

    Fix dark mode styles

--- components/Accordion.js
+++ components/Accordion.js
@@ -4,11 +4,11 @@ const Accordion = ({ title, text, opened }) => {
 	const [isOpen, setIsOpen] = useState(opened || false);
 
 	return (
-		<div className="relative z-10 overflow-hidden 	 md:w-2/3">
+		<div className="relative z-10 mb-4 overflow-hidden md:w-2/3">
 			<div
 				onClick={() => setIsOpen(!isOpen)}
-				className={`flex w-full cursor-pointer items-center rounded py-4 text-xl   font-semibold transition-all duration-300 ease-in-out hover:bg-slate-200 dark:hover:text-neutral-800 ${
-					isOpen && "bg-slate-200 "
+				className={`flex w-full cursor-pointer items-center rounded py-4 text-xl   font-semibold transition-all duration-300 ease-in-out dark:hover:bg-gray-800 ${
+					isOpen && "bg-slate-200 dark:bg-gray-800"
 				}`}
 			>
 				<span
@@ -21,8 +21,8 @@ const Accordion = ({ title, text, opened }) => {
 				<p>{title}</p>
 			</div>
 			<div
-				className={`w-full rounded-b-lg  bg-slate-100  p-8 transition-all duration-200  ease-in-out
-					${!isOpen && "absolute -translate-y-96 pt-2"}
+				className={`w-full rounded-b-lg  bg-slate-100 p-8 transition-all duration-200 ease-in-out  dark:bg-gray-600
+					${!isOpen && "absolute -translate-y-96 pt-2 "}
 				`}
 			>
 				<p>{text}</p>
