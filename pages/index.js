90565462c90f21e3c86be872ef71c04743a56e0a
Omar Sakr <o.sakr@ptscorp-eg.com>
2022-10-16 23:46:36 +0200

    temporary hide blog room section

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
 				<Subsidiaries />
 				<TargetMarkets />
 				<Solutions />
-				<BlogRoom />
+				{/* <BlogRoom /> */}
 				<Faq />
 				<ContactForm />
 			</div>
